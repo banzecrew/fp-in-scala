4e434a0434b9cd5f09b9b8d20c36491720800b25
hex <dkrykovwork@gmail.com>
2021-04-11 19:58:36 +0300

    feat: exercise 2.2

--- src/main/scala/fp/gettingstarted/GettingsStarted.scala
+++ src/main/scala/fp/gettingstarted/GettingsStarted.scala
@@ -13,4 +13,15 @@ object GettingsStarted {
 
     go(n, 0, 1)
   }
+
+  //2.2
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) loop(n + 1)
+      else false
+
+    loop(0)
+  }
 }
