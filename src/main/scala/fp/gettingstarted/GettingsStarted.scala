b2480cb4d0ff82fac553a67f7e8573d4492a6831
hex <dkrykovwork@gmail.com>
2021-04-11 21:43:55 +0300

    feat: exercise 2.4

--- src/main/scala/fp/gettingstarted/GettingsStarted.scala
+++ src/main/scala/fp/gettingstarted/GettingsStarted.scala
@@ -28,4 +28,8 @@ object GettingsStarted {
   //2.3[X]
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
     a => b => f(a, b)
+
+  //2.4[ ]
+  def uncurry[A,B,C](f: A => (B => C)): (A, B) => C =
+    (a, b) => f(a)(b)
 }
