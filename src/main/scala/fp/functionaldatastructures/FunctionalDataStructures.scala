6a40d1b7c7ea3956e7ee442ad41abc37f72d4d3b
hex <dkrykovwork@gmail.com>
2021-04-18 20:44:36 +0300

    feat: exercise 3.25

--- src/main/scala/fp/functionaldatastructures/FunctionalDataStructures.scala
+++ src/main/scala/fp/functionaldatastructures/FunctionalDataStructures.scala
@@ -199,3 +199,15 @@ object List {
     case (Nil, Cons(h, t)) => false
   }
 }
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+object Tree {
+
+  //3.25[X]
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(value) => 1
+    case Branch(l, r) => size(l) + size(r) + 1
+  }
+}
