be62be700d5e59675b4453114f5efa81c5fcee4c
hex <dkrykovwork@gmail.com>
2021-04-24 18:23:07 +0300

    feat: exercise 4.1

---
+++ src/main/scala/fp/handlingerrorswithoutexceptions/Option.scala
@@ -0,0 +1,30 @@
+package fp.handlingerrorswithoutexceptions
+
+sealed trait Option[+A] {
+  //4.1[X]
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(v) => Some(f(v))
+    case _       => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(v) => v
+    case _       => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(v) if (f(v)) => this
+    case _                 => None
+  }
+
+  def filter2(f: A => Boolean): Option[A] =
+    flatMap(x => if (f(x)) Some(x) else None)
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
