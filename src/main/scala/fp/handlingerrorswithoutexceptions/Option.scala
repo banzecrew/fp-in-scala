954591c2236fca269676d25425deb01c3e242b38
hex <dkrykovwork@gmail.com>
2021-04-25 21:56:58 +0300

    feat: exercise 4.2

--- src/main/scala/fp/handlingerrorswithoutexceptions/Option.scala
+++ src/main/scala/fp/handlingerrorswithoutexceptions/Option.scala
@@ -26,5 +26,14 @@ sealed trait Option[+A] {
   def filter2(f: A => Boolean): Option[A] =
     flatMap(x => if (f(x)) Some(x) else None)
 }
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  //4.2[X]
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+}
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
