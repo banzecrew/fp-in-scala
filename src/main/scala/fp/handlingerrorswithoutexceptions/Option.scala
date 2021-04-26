6c9dcfc046b94ee5881c4bcd0dac85622af97bb0
hex <dkrykovwork@gmail.com>
2021-04-27 01:21:03 +0300

    feat: exercise 4.5

--- src/main/scala/fp/handlingerrorswithoutexceptions/Option.scala
+++ src/main/scala/fp/handlingerrorswithoutexceptions/Option.scala
@@ -1,5 +1,8 @@
 package fp.handlingerrorswithoutexceptions
 
+import scala.{Option => _, Either => _, _}
+
+
 sealed trait Option[+A] {
   //4.1[X]
   def map[B](f: A => B): Option[B] = this match {
@@ -67,6 +70,18 @@ object Option {
   def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
     case Nil    => Some(Nil)
+  }
+
+  def parseInts(a: List[String]): Option[List[Int]] =
+    sequence(a map (i => Try(i.toInt)))
+
+  //4.5[X]
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    sequence (a map f)
+
+  def traverse2[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case h :: t => map2(f(h), traverse2(t)(f))(_ :: _)
+    case Nil => Some(Nil)
   }
 }
 case class Some[+A](get: A) extends Option[A]
