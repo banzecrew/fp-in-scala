15e4f0ba16d8763d10a2339bf3019d71fa256424
hex <dkrykovwork@gmail.com>
2021-05-23 02:30:06 +0300

    feat: exercise 5.2

--- src/main/scala/fp/strictnessandlazines/Stream.scala
+++ src/main/scala/fp/strictnessandlazines/Stream.scala
@@ -11,6 +11,16 @@ sealed trait Stream[+A] {
     case Cons(h, t) => h() :: t().toList
     case _ => Nil
   }
+
+  //5.2[X]
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if (n > 0) => Cons(() => h(), () => t().take(n - 1))
+    case _ => Empty
+  }
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if (n > 0) => t().drop(n - 1)
+    case _ => this
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
